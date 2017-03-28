d977313e87476605fe153427f87c76fbd80c4b00
kscottz <katherineAScott@gmail.com>
2017-03-28 21:34:26 +0000

    12.04 Settings.

---
+++ setup_ubuntu_12.sh
@@ -0,0 +1,10 @@
+sudo add-apt-repository ppa:ubuntu-elisp/ppa
+sudo apt-get update
+sudo apt-get install emacs-snapshot emacs-snapshot-el
+sudo apt-get install byobu
+sudo apt-get install pyflakes
+alias emacs="emacs-25.1.50"
+alias ls="ls -flah" 
+git config --global user.email "katherineAScott@gmail.com"
+git config --global user.name "kscottz"
+cp ./dot_emacs_1204.txt ~/.emacs 
