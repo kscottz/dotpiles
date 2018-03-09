729334f145952efd0647eb464e1e1d9df6dd5c8c
Katherine Scott <katherineAScott@gmail.com>
2018-03-09 13:45:05 -0800

    More vlc and curl

--- install_desktop.sh
+++ install_desktop.sh
@@ -1,10 +1,15 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update 
+sudo apt-get install curl
+curl ftp://ftp.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
+echo "deb ftp://ftp.videolan.org/pub/debian/stable ./" | sudo tee /etc/apt/sources.list.d/libdvdcss.list
+sudo apt-get update
+sudo apt-get install vlc vlc-data browser-plugin-vlc mplayer2
+sudo apt-get install vlc-plugin-pulse
 sudo apt-get install google-chrome-stable
-sudo apt-get install git emacs htop aptitude gimp vlc gimp-plugin-registry ipython python2.7 libopencv-dev python-opencv python-opencv-apps python2.7-opencv python2.7-numpy python2.7-sklearn-lib imagej libav-tools byobu pyflakes imagemagick  
-sudo apt-get install ubuntu-restricted-extras 
+sudo apt-get install git emacs htop aptitude gimp gimp-plugin-registry ipython python2.7 libopencv-dev python-opencv python-opencv-apps python2.7-opencv python2.7-numpy python2.7-sklearn-lib imagej libav-tools byobu pyflakes imagemagick  
 alias ls="ls -flah" 
 git config --global user.email "katherineAScott@gmail.com"
 git config --global user.name "kscottz"
 cp ./dot_emacs_1204.txt ~/.emacs 
+sudo apt-get install ubuntu-restricted-extras 
