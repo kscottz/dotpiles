2145fd14667c83485bb194a573362f218cd875b9
Katherine Scott <katherineAScott@gmail.com>
2018-03-09 13:36:44 -0800

    Update install_desktop.sh
    Added codecs and stuff.

--- install_desktop.sh
+++ install_desktop.sh
@@ -3,6 +3,7 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 sudo apt-get update 
 sudo apt-get install google-chrome-stable
 sudo apt-get install git emacs htop aptitude gimp vlc gimp-plugin-registry ipython python2.7 libopencv-dev python-opencv python-opencv-apps python2.7-opencv python2.7-numpy python2.7-sklearn-lib imagej libav-tools byobu pyflakes imagemagick  
+sudo apt-get install ubuntu-restricted-extras 
 alias ls="ls -flah" 
 git config --global user.email "katherineAScott@gmail.com"
 git config --global user.name "kscottz"
